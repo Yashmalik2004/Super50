84edd63b85f97490e5507b0d661ad401dc53987b
Yashmalik2004 <yashmalik4832004@gmail.com>
2026-03-31 14:06:09 +0530

    palindrome partioning of a string

---
+++ DSA/30mar2026/palindrome_partitioning.cpp
@@ -0,0 +1,27 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// class Solution {
+// public:
+//     bool ispalindrome(string s, int i, int j){
+//         if(s[i]!=s[j]) return false;
+//         return true;
+//     }
+//     void solve(string&s){
+//         int n=s.size();
+//         for(int j=i;j<n;j++){
+//             if(ispalindrome)
+//         }
+
+//     }
+//     vector<vector<string>> partition(string s) {
+//         vector<vector<string>> ans;
+//         vector<string> curr;
+//         solve(0,s,ans,curr);
+//         return ans;
+//     }
+// };
+
+// int main(){
+//   return 0;
+// }
