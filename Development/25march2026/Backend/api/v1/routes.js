cba8a9ea5f38a30b83591fafe254f51148735756
Yashmalik2004 <yashmalik4832004@gmail.com>
2026-04-01 11:22:24 +0530

    new commit

--- Development/25march2026/Backend/api/v1/routes.js
+++ Development/25march2026/Backend/api/v1/routes.js
@@ -19,12 +19,28 @@ v1Router.post("/players", async(req,res) => {
     },
   });
   }catch(err){
-    console.log("Error in creating player", err.message);
-    res.status(500);
-    res.json({
-      success: false,
-      message: "Bhaiya ji apka code me error hai, player create nahi ho paya",
-    })
+    if(err.name === "ValidationError"){
+        res.status(400);
+        res.json({
+        success: false,
+        message: "validation failed, please check your input data"+err.message,
+      });
+    } else if(err.name === "MongoServerError" && err.code === 11000){
+       res.status(400);
+      res.json({
+        success: false,
+        message: "Duplicate key error: A player with this username or email already exists."+err.message,
+      });
+    }else{
+
+      console.log(err.name);
+      console.log("Error in creating player", err.message);
+      res.status(500);
+      res.json({
+        success: false,
+        message: "Bhaiya ji apka code me error hai, player create nahi ho paya",
+      });
+    }
   }
 });
 
