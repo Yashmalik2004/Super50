89dca4f7b62d7b389aae7d23202f486499eb19e8
Yashmalik2004 <yashmalik4832004@gmail.com>
2026-04-04 22:14:26 +0530

    dsa practice for contest

--- DSA/contest-11_4-4-26/The 67th OEIS Problem.cpp
+++ DSA/contest-11_4-4-26/The 67th OEIS Problem.cpp
@@ -7,18 +7,14 @@ int main() {
     while (t--) {
         int n;
         cin >> n;
-
         vector<long long> a(n);
-
         for (int i = 0; i < n; i++) a[i] = i + 1;
 
         for (int i = 0; i + 1 < n; i += 2) {
             swap(a[i], a[i + 1]);
         }
-
         for (auto x : a) cout << x << " ";
         cout << '\n';
     }
-
     return 0;
 }
