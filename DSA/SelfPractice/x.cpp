ad2f1afc9da31ac78876e242124d19c9ac4f7ea2
Yashmalik2004 <yashmalik4832004@gmail.com>
2026-04-06 23:34:12 +0530

    dsa

---
+++ DSA/SelfPractice/x.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int func(int i, vector<int>&arr, vector<int>&curr, vector<vector<int>>&ans){
+  int n=arr.size();
+    if(i==n){
+      ans.push_back(curr);
+      return;
+    }
+}
