97fc310bc7a92d89dc322959def188e3fae5c542
Yashmalik2004 <yashmalik4832004@gmail.com>
2026-04-09 10:33:04 +0530

    9 april

--- DSA/9april2026/bitPractice.cpp
+++ DSA/9april2026/bitPractice.cpp
@@ -3,12 +3,14 @@ using namespace std;
 
 int main(){
   int n=16;
-  for(int i=0;i<32;i++){
-    if(n&(1<<i)){
-      cout<<i;
-      break;
-    }
-    else continue;
-  }
+  // for(int i=0;i<32;i++){
+  //   if(n&(1<<i)){
+  //     cout<<i;
+  //     break;
+  //   }
+  //   else continue;
+  // }
+  n=n&(n-1);
+  cout<<n;
   return 0;
 }
