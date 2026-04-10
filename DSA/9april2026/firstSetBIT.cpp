34fabf4eb7e4670d56bcdc6e1162be9d03d80cd1
Yashmalik2004 <yashmalik4832004@gmail.com>
2026-04-11 00:08:04 +0530

    git work

--- DSA/9april2026/firstSetBIT.cpp
+++ DSA/9april2026/firstSetBIT.cpp
@@ -1,19 +1,3 @@
-// #include<bits/stdc++.h>
-// using namespace std;
-
-// int main(){
-//   int n;
-//   cin>>n;
-//   int msbPos=0;
-//   while(n>1){
-//     n=n>>1;
-//     msbPos++;
-//   }
-//   cout<<msbPos<<endl;
-//   return 0;
-// }
-
-
 
 #include <bits/stdc++.h>
 using namespace std;
