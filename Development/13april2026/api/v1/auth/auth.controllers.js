ce94c8c4cf580c69bc951d84a9fb607954520437
Yashmalik2004 <yashmalik4832004@gmail.com>
2026-04-15 11:33:51 +0530

    9april

--- Development/13april2026/api/v1/auth/auth.controllers.js
+++ Development/13april2026/api/v1/auth/auth.controllers.js
@@ -5,13 +5,11 @@ const signupController = async (req, res) => {
     try {
         const { email, password } = req.body;
 
-        // if (!email || !password) {
-        //     return res.status(400).json({ message: "Email and password are required" });
-        // }
-
         const existingUser = await User.findOne({ email });
+
         if (existingUser) {
-            return res.status(400).json({ message: "User already exists" });
+            res.status(400).json({ message: "User already exists" });
+            return;
         }
 
         const hashedPassword = await bcrypt.hash(password, 10);
