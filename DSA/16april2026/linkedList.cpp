e162ed500daeae1bae69ed94a2a5af672a700a1b
Yashmalik2004 <yashmalik4832004@gmail.com>
2026-04-16 23:07:26 +0530

    LL

--- DSA/16april2026/linkedList.cpp
+++ DSA/16april2026/linkedList.cpp
@@ -27,23 +27,23 @@ int main()
   }
   temp->next = new Node(50);
   temp = head;
-
+//------------------------------------------------
   // insert node at front
   Node *newNode = new Node(1000);
   newNode->next = head;
   head = newNode;
 
   temp = newNode;
-
+  
   while (temp != nullptr)
   {
     cout << temp->data << " --> ";
     temp = temp->next;
   }
-  cout << "NULL :)";
+  cout << "NULL :) Added 1000 at front";
   cout << endl;
   cout << endl;
-
+//------------------------------------------------
   temp = head;
 
   // insert in middle
@@ -55,17 +55,17 @@ int main()
   Node *middlenode = new Node(100);
   middlenode->next = temp->next;
   temp->next = middlenode;
-
+//------------------------------------------------
   temp = head;
   while (temp != nullptr)
   {
     cout << temp->data << " --> ";
     temp = temp->next;
   }
-  cout << "NULL :)";
+  cout << "NULL :) Inserted 100 before 2";
   cout << endl;
   cout << endl;
-
+//------------------------------------------------
   temp = head;
   // deletion at last
   while (temp->next->next != nullptr)
@@ -73,6 +73,7 @@ int main()
     temp = temp->next;
   }
   Node *delnode = temp->next; // just to delete the node (optional)
+  delete delnode;
   temp->next = nullptr;
 
   temp = head;
@@ -81,9 +82,49 @@ int main()
     cout << temp->data << " --> ";
     temp = temp->next;
   }
-  cout << "NULL :)";
+  cout << "NULL :) Last node gone";
   cout << endl;
   cout << endl;
+//------------------------------------------------
+  Node* delNode = head;
+  // deletion at front
+
+  head = head->next;
+  temp = head;
+  while(temp!=nullptr){
+    cout<<temp->data<<" --> ";
+    temp = temp->next;
+  }
+  cout<<"NULL :) Front node gone";
+  cout<<endl;
+  cout<<endl;
+  delete delNode;
+
+ //------------------------------------------------ 
+  // adding a node at start for practice
+  Node* newNode2 = new Node(10000);
+  newNode2->next = head;
+  head = newNode2;
+  temp = head;
+  while(temp!=nullptr){
+    cout<<temp->data<<" --> ";
+    temp = temp->next;
+  }
+  cout<<"NULL :) Added a node at start for practice";
+  cout<<endl;
+  cout<<endl;
+
+//------------------------------------------------
+  // Now deleting the 2nd node
+  temp = head;
+  head->next = head->next->next;
+  while(temp!=nullptr){
+    cout<<temp->data<<" --> ";
+    temp = temp->next;
+  }
+  cout<<"NULL :) 2nd node gone";
+  cout<<endl;
+  cout<<endl;
 
   return 0;
 }
