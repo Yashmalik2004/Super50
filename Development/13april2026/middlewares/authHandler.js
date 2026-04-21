4cbc866271d2b903c5d8289e332af0236e9bcb21
Yashmalik2004 <yashmalik4832004@gmail.com>
2026-04-21 14:43:39 +0530

    Auth check

--- Development/13april2026/middlewares/authHandler.js
+++ Development/13april2026/middlewares/authHandler.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 
-const validateUser = (req, res, next) =>{
-   const token = req.cookies?.authorization;
-   if(!token) res.status(400).json({message:"Unauthorized"});
-
-   const decode = jwt.verify(token, process.env.JWT_SECRET);
-}
+const validateUser = (req, res, next) => {
+  const token = req.cookies?.authorization;
+  if (!token) res.status(400).json({ success: false, message: "Unauthorized" });
+  try {
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(400).json({ success: false, message: "Unauthorized" });
+    return;
+  }
+};
